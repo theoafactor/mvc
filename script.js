d1ba0a3413917e6eef85e5b1240dca9ef4deeb70
Olu <theoafactor@gmail.com>
2022-06-27 13:18:41 +0100

    Added Views

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function aboutus(){
-
+    console.log("This is about us")
+    loadPageView(page);
 }
 
 function contactus(){
+    console.log("Show contact us page")
 
+    loadPageView(page);
 
 }
 
 function register(param1, param2){
-
+    console.log("show register user")
+    loadPageView(page);
 }
+
 console.log(location);
 
 let locationData = location.href;
@@ -20,6 +25,26 @@ const paths = locationData.split("?");
 const page = paths[1];
 
 console.log(page)
+
+window[page]()
+
+
+function loadPageView(pagename){
+    const xhr = new XMLHttpRequest;
+
+    //open
+    xhr.open("GET", `http://localhost:5501/views/_${pagename}.html`);
+
+    //load
+    xhr.addEventListener("load", function(){
+        // console.log(xhr.responseText);
+        document.body.innerHTML = xhr.response;
+    })
+
+    //send
+    xhr.send()
+}
+
 
 
 
@@ -32,3 +57,4 @@ console.log(page)
 
 // aboutus();
 // contactus();
+
