c102219bad3e4e96457011131f16b9af72dd7525
Olu <theoafactor@gmail.com>
2022-06-27 12:26:22 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function aboutus(){
+
+}
+
+function contactus(){
+
+
+}
+
+function register(param1, param2){
+
+}
+console.log(location);
+
+let locationData = location.href;
+
+//lets extract the right function
+const paths = locationData.split("?");
+
+const page = paths[1];
+
+console.log(page)
+
+
+
+
+//call the function that matches the page
+
+
+
+
+
+// aboutus();
+// contactus();
